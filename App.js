1f84c39ce1087a38e329af37b26c254229a47c99
kalavh <jefferson_1996Th@hotmail.com>
2020-05-17 18:16:30 -0300

    start project

---
+++ App.js
@@ -0,0 +1,23 @@
+// In App.js in a new project
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/pages/Home'
+import Login from './src/pages/Login'
+
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Login" component={Login}    />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+export default class Home extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  render() {
+    return (
+      <View style={styles.teste}>
+        
+        <Text> Home </Text>
+      </View>
+    );
+  }
+}
+
+const styles =  StyleSheet.create ({
+  teste: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#000'
+  },
+})
+ 
