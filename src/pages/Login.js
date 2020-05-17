ec2b1b2cf6d8168f79eaf88643534642e375d6ea
kalavh <jefferson_1996Th@hotmail.com>
2020-05-17 19:57:56 -0300

    base of login screen

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,14 +1,22 @@
 import React, { Component } from 'react';
-import { View, Text, StyleSheet,Button } from 'react-native';
-import { TouchableOpacity } from 'react-native-gesture-handler';
+import { View, Text, StyleSheet, Button, TextInput } from 'react-native';
 
 function Login ({navigation} ) {
     return (
         <View style={styles.container} >
-          <Text> Login </Text>
-          <Button title= "Login"
-           onPress= {() => navigation.navigate('Home') } 
-           />
+            <View style={styles.moldura} >
+
+    
+                <TextInput style={styles.input}  placeholder=" Digite Seu usuario" />
+               
+            
+                <TextInput style={styles.input}  placeholder=" Digite Seu usuario" />
+                
+                <Button style={styles.login} title= "Login"
+                onPress= {() => navigation.navigate('Home') } 
+                />
+
+            </View>
         </View>
       );
 }
@@ -16,9 +24,32 @@ function Login ({navigation} ) {
 
 const styles = StyleSheet.create({
     container:{
+        alignItems: 'center',
+        justifyContent: 'center',
         flex: 1,
         backgroundColor: '#fafafa',
     } ,
+    moldura: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        width: 300,
+        height: 350,
+        backgroundColor: '#fff',
+        borderRadius: 10,
+        elevation: 3,
+    } ,
+    input:{
+        borderWidth: 0.3,
+        borderRadius: 4,
+        width: '90%',
+        margin: 9,
+
+    },  
+    login: {
+        margin: 30,
+        width:'90%',
+    }
+ 
 
 })
 
