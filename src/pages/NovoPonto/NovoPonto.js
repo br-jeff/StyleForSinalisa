cbdc1641c8fc8338144dab15d0c8e3b4e116bbe8
kalavh <jefferson_1996Th@hotmail.com>
2020-05-22 20:31:21 -0300

    add date,hour and just let aceppt numbers in idfunc

--- src/pages/NovoPonto/NovoPonto.js
+++ src/pages/NovoPonto/NovoPonto.js
@@ -2,8 +2,36 @@ import React from 'react';
 import { View,Text, StyleSheet, Button, TextInput, TouchableOpacity } from 'react-native';
 
 function NovoRelatorio() {
+    var dataDia = new Date().getDate() 
+    var dataMes = new Date().getMonth() 
+    var dataAno = new Date().getFullYear() 
+    var tempoHora = new Date().getHours() 
+    var tempoMinutos = new Date().getMinutes()
+    return(
 
-    return(<Text> Novo Ponto Eletronico </Text> )
+        <View style={styles.container}>
+            <TextInput keyboardType={'numeric'} style={styles.idFunc} type="cc-number" placeholder="Codigo do funcionario" />
+           
+    <Text> Data: {dataDia}/{dataMes}/{dataAno} </Text>
+    <Text> Hora: {tempoHora}:{tempoMinutos} </Text>
+    
+    </View>
+    )
+
 }
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    idFunc: {
+        borderWidth: 0.4,
+        borderRadius: 30,
+        width: 250,
+        textAlign: 'center'
+
+    }
+})
 
 export default NovoRelatorio
